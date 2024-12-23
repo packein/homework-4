879f5b63742c0e8fdd01c23800eff1c903ebb04f
packein <136176264+packein@users.noreply.github.com>
2024-12-24 03:47:01 +0900

    Create kakao.vue

---
+++ src/vue/kakao.vue
@@ -0,0 +1,14 @@
+methods: {
+  kakaoLogin() {
+    Kakao.Auth.login({
+      success: (authObj) => {
+        console.log('로그인 성공:', authObj);
+        alert('카카오 로그인에 성공했습니다!');
+      },
+      fail: (error) => {
+        console.error('로그인 실패:', error);
+        alert('카카오 로그인에 실패했습니다.');
+      },
+    });
+  }
+}
